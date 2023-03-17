d424e699270f7dd3193a0dde364b60b4f5b32ae6
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-03-17 20:19:31 +0530

    create new activity file

---
+++ app/src/main/java/com/example/demosenserpractical01/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.demosenserpractical01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity2 extends AppCompatActivity implements SensorEventListener {
+
+    private TextView textView;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private float changedValues;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
